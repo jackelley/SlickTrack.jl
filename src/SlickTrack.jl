0c09ea586be43cc62f04cdac3f5a310ec6adb5db
jackelley <jack.kelley1201@gmail.com>
2025-06-03 15:41:40 -0600

    create some boilerplate

--- src/SlickTrack.jl
+++ src/SlickTrack.jl
@@ -1,5 +1,12 @@
 module SlickTrack
 
+using LinearAlgebra, StaticArrays, Quaterions, Beamlines
+
 println("Hello from SlickTrack!")
+
+include("spin.jl")
+include("track.jl")
+include("types.jl")
+include("io.jl")
 
 end
